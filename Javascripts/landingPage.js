b61089ec08cf9ed3f248f8db29cc699c1d001f3f
Zacharycisney <116757784+Zacharycisney@users.noreply.github.com>
2024-01-03 17:37:12 -0700

    small changes for background color

--- Javascripts/landingPage.js
+++ Javascripts/landingPage.js
@@ -2,12 +2,12 @@
 
 // Javascript linking test (passed)
 // window.addEventListener("keydown", event => {
-//       if (event.key == "v") {
+//       if (event.key == "d") {
 //         document.body.style.background = "violet";
 //       }
 //     });
 //     window.addEventListener("keyup", event => {
-//       if (event.key == "v") {
+//       if (event.key == "d") {
 //         document.body.style.background = "";
 //       }
 //     });
